54cf5c8cc4a3a13115a57da85f6d76e13abf4df0
Flzzyf <flzzyf076@gmail.com>
2018-03-01 14:02:53 +0800

    信息UI自动上下移动

--- ZyfTD/Assets/InfoUI.cs
+++ ZyfTD/Assets/InfoUI.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class InfoUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool uiAbove = true;
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        Vector3 mousePoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+        if(MouseAbove() == uiAbove)
+        {
+            Debug.Log("鼠标在上");
+            uiAbove = !uiAbove;
+
+            SwitchUIPos();
+
+        }
+    }
+
+    //切换UI位置
+    void SwitchUIPos()
+    {
+        Vector3 newPos = transform.position;
+        newPos.z *= -1;
+        transform.position = newPos;
+    }
+
+    //判断鼠标在屏幕上部
+    bool MouseAbove()
+    {
+        return Input.mousePosition.y > Screen.height / 2;
+    }
 }
